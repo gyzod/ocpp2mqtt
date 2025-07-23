70e7ca1f9373cb23f8581bafa33a7f08cd2d194c
Nodiaque <nodiaque@msn.com>
2025-07-23 11:29:29 -0400

    Update charge_point.py

--- charge_point.py
+++ charge_point.py
@@ -180,20 +180,23 @@ class ChargePoint(cp):
     # MQTT implementation
     ## MQTT publish
     async def push_state_values_mqtt(self,**kwargs):
-        for k,v in kwargs.items():
-            mqtt_path = self.get_mqttpath()
-            print("path: ", mqtt_path)
+        mqtt_path = self.get_mqttpath()
+        print("path1: ", mqtt_path)
+        for k,v in kwargs.items():    
+            print("value1: ", k)
             await self.client.publish(f"{mqtt_path}/state/{k}", payload=v)
 
     async def push_state_value_mqtt(self, key, value):
         mqtt_path = self.get_mqttpath()
-        print("path: ", mqtt_path)
+        print("path2: ", mqtt_path)
+        print("value2: ", key)
         await self.client.publish(f"{mqtt_path}/state/{key}", payload=value)
 
     async def push_call_return_mqtt(self, result):
+        mqtt_path = self.get_mqttpath()
+        print("path3: ", mqtt_path)
         for k,v in result.items():
-            mqtt_path = self.get_mqttpath()
-            print("path: ", mqtt_path)
+            print("value3: ", k)
             await self.client.publish(f"{mqtt_path}/cmd_result/{k}", payload=v)
 
     ## received events from MQTT
